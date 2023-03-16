629d9af0c6d65e9e38d0be79a35e5a955e0690b8
Oleksandr Kostiushko <oleksandr.kostiushko@oa.edu.ua>
2023-03-17 00:27:08 +0200

    Update CurrentProjectCard.js

--- src/components/project-cards/CurrentProjectCard.js
+++ src/components/project-cards/CurrentProjectCard.js
@@ -27,15 +27,7 @@ function ProjectCard(props) {
             31.01.2022–25.02.2022<br/>
             Worked as a front-end programmer, the main task was the development of new blocks and the layout of new pages.
             </Typography>
-          </CardContent>
-        </Card>
-      </Grid>
-    </Grid>
 
-<Grid container spacing={24} alignContent='center' alignItems='center' justify='center'>
-      <Grid item xs={7}>
-        <Card className={classes.card}>
-          <CardContent>
             <Typography type="headline" component="h2">
             IT HUB React.js developer
             </Typography>
